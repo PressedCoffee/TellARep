da52c526c64ea403bb59964861576b6f4f1a6e5b
lkmSasanga <lkmsasanga@gmail.com>
2023-09-06 09:45:36 +0530

    Add current zip code

--- tellarep/src/components/Profile/Profile.jsx
+++ tellarep/src/components/Profile/Profile.jsx
@@ -143,7 +143,7 @@ function Profile({ user }) {
       {userData && (
         <div>
           <p>Email: {userData.email}</p>
-          {/* {zipCode && <p>Current Zip Code: {zipCode}</p>} */}
+          {zipCode && <p>Current Zip Code: {zipCode}</p>}
           <ZipCodeComponent onSubmit={handleZipCodeSubmit} />
           {/* ... rest of the JSX code ... */}
           {data && !repsLoading ? (
